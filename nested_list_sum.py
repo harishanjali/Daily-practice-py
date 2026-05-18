8fb25833b31dd79e1f4e28b5f3b98ebd09dc31ee
harish <harikrishnapuram198@gmail.com>
2026-05-18 18:02:43 +0530

    code structure for program

--- nested_list_sum.py
+++ nested_list_sum.py
@@ -1,4 +1,12 @@
 x = [[1,2,3],[4,5],[6,7],8]
 
-
-
+def flatlist(nst_list):
+    numbers = []
+    if isinstance(nst_list,list):
+        for sub_list in nst_list:
+             numbers += flatlist(sub_list)
+    elif isinstance(nst_list,int):
+        numbers.append(nst_list)
+    return numbers
+res = flatlist(x)
+print(res)
